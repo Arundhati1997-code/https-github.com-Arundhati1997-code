2f6d8ad1b75384352d51b24b2a560bbb3dffda36
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2021-08-30 20:37:58 +0530

    Create postivenos.py

---
+++ postivenos.py
@@ -0,0 +1,29 @@
+list1 = [12, -7, 5, 64, -14]
+ 
+# iterating each number in list
+for num in list1:
+     
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+
+        
+        output:
+          12 5 64 
+          
+          
+          
+          list2 = [12, 14, -95, 3]
+ 
+# iterating each number in list
+for num in list2:
+     
+    # checking condition
+    if num >= 0:
+       print(num, end = " ")
+        
+        
+        Output:
+          12 14 3 
+
+

691b28b414efec47149b7090475b8be3064ae39b
Arundhati1997-code <88510301+Arundhati1997-code@users.noreply.github.com>
2021-12-01 20:54:10 +0530

    Create a14.py

---
+++ a14.py
@@ -0,0 +1,34 @@
+# Python program to print positive Numbers in a List
+ 
+# list of numbers
+list1 = [12, -7, 5, 64, -14]
+
+# iterating each number in list
+for num1 in list1:
+    
+    # checking condition
+      if num1 >= 0:
+       print(num1, end = " ")
+
+      
+      
+      Output
+      12 5 64
+      
+      
+
+# Python program to print positive Numbers in a List
+list2 = [12, 14, -95, 3]
+ # iterating each number in list
+for   num2 in list2:
+     
+
+     # checking condition
+    if num2 >= 0:
+       print(num2, end = " ")
+        
+        
+        
+        Output:
+         12 14 3 
+
